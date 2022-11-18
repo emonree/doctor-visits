6b68f6d49fd388bd1ff5d88c4ce5c2999fb45a92
Emon Li <emonli.328@gmail.com>
2022-11-18 10:30:00 -0500

    created schema

--- models/entrySchema.js
+++ models/entrySchema.js
@@ -1,12 +1,17 @@
 const mongoose = require('mongoose');
 
 const entrySchema = new mongoose.Schema({
-    name: String,
-    species: String,
-    img: String,
-    breed: String,
-    age: String,
-    sex: String,
+    name: { type: String, required: true },
+    birthday: { type: String, required: true },
+    gender: { type: String, required: false },
+    height: { type: String, required: false },
+    weight: { type: String, required: false },
+    drugalleries: { type: String, required: true },
+    medicalconditions: { type: String, required: true },
+    rxlist: { type: String, required: true },
+    rxchanges: { type: String, required: false },
+    drorders: { type: String, required: true },
+    
 })
 
 const entryCollection = mongoose.model('Entries', entrySchema);
