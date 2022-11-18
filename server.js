4c3f62f6bf1fa79ac5a9d67752fd18f6c10cc2a4
Emon Li <emonli.328@gmail.com>
2022-11-17 20:38:04 -0500

    connecting to mongo

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose')
 
 let PORT = 3000;
 if(process.env.PORT) {
@@ -13,3 +14,7 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log('listening...');
 });
+
+mongoose.connect('mongodb+srv://emonga:aBB4E9oTaQ48nM&Y@cluster0.0eubsxh.mongodb.net/?retryWrites=true&w=majority', () => {
+    console.log('connected to mongo')
+})
